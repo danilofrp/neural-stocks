6a4c4ad18a59a5e410c22626a7f28b3f59a82129
Danilo Filippo <danilo.frp@poli.ufrj.br>
2018-04-11 15:32:11 -0300

    TrainMultiple updated for SAE

--- src/SAE/TrainMultiple.py
+++ src/SAE/TrainMultiple.py
@@ -3,14 +3,16 @@ import click
 
 @click.command()
 @click.option('--asset', '-a', multiple=True)
+@click.option('--force', is_flag = True, help = 'Forces new trainings even if previously trained models exist.')
 @click.option('--msg/--no-msg', default = False, help = 'Enables/disables telegram messaging. Defalut False')
 @click.option('--dev', is_flag = True, help = 'Development flag')
-def main(asset, msg, dev):
+def main(asset, force, msg, dev):
     norms = ['mapminmax', 'mapstd']
     for a in asset:
         for norm in norms:
-            args = ['python', 'TrainMLP.py', '--asset={}'.format(a), '--inits=10', '--norm={}'.format(norm), '--optimizer=adam', '--verbose']
+            args = ['python', 'TrainSAE.py', '--asset={}'.format(a), '--inits=10', '--norm={}'.format(norm), '--optimizer=adam', '--verbose']
             args.append('--msg') if msg else args.append('--no-msg')
+            if force: args.append('--force')
             if dev: args.append('--dev')
             #print args
             p = subprocess.Popen(args)
