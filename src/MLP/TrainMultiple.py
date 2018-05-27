d9cab559d5025fce9e07c1c4bbdf15ea16131095
Danilo Filippo <danilo.frp@poli.ufrj.br>
2018-05-27 14:00:43 -0300

    Multiple MLP (with CV) training support

--- src/MLP/TrainMultiple.py
+++ src/MLP/TrainMultiple.py
@@ -3,13 +3,17 @@ import click
 
 @click.command()
 @click.option('--asset', '-a', multiple=True)
+@click.option('--cv', is_flag=True)
 @click.option('--msg/--no-msg', default = False, help = 'Enables/disables telegram messaging. Defalut False')
 @click.option('--dev', is_flag = True, help = 'Development flag')
 def main(asset, msg, dev):
     norms = ['mapminmax', 'mapstd']
     for a in asset:
         for norm in norms:
-            args = ['python', 'TrainMLP.py', '--asset={}'.format(a), '--inits=10', '--norm={}'.format(norm), '--optimizer=adam', '--verbose']
+            if cv:
+                args = ['python', 'TrainMLPwithCV.py', '--asset={}'.format(a), '--inits=10', '--norm={}'.format(norm), '--optimizer=adam', '--verbose']
+            else:
+                args = ['python', 'TrainMLP.py', '--asset={}'.format(a), '--inits=10', '--norm={}'.format(norm), '--optimizer=adam', '--verbose']
             args.append('--msg') if msg else args.append('--no-msg')
             if dev: args.append('--dev')
             #print args
